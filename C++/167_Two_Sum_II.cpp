0740e019c5e6986368636a588660ae852d9bf16b
Ethan Yu <lambertyuguo@gmail.com>
2016-11-27 22:27:30 -0500

    167_two_sum_solution
    The array is already sorted in ascending order.

--- C++/167_Two_Sum_II.cpp
+++ C++/167_Two_Sum_II.cpp
@@ -1,15 +1,17 @@
 /*
- *Given an array of integers, return indices of the two numbers such that they add up to a specific target.
- *You may assume that each input would have exactly one solution.
- *Example:
- *Given nums = [2,3,4,5,6,9,11,23,30,32], target = 10,
- *Because nums[2] + nums[4] = 4 + 6 = 10,
- *return [2, 4].
- *UPDATE (2016/11/27):
- *The return format had been changed to zero-based indices. Please read the above updated description carefully
- *Tag: Array, Hash Table
- 
- *Author: Ethan YU
+*Given an array of integers that is already sorted in ascending order, 
+*find two numbers such that they add up to a specific target number.
+*The function twoSum should return indices of the two numbers such that 
+*they add up to the target, where index1 must be less than index2. 
+*Please note that your returned answers (both index1 and index2) are not zero-based.
+
+*You may assume that each input would have exactly one solution.
+
+*Input: numbers={2, 7, 11, 15}, target=9
+*Output: index1=1, index2=2
+
+*Tag: Array, Two Pointers, Binary Search
+*Author: Ethan YU
 */
 
 #include <vector>
@@ -23,21 +25,16 @@ public:
 /*
 Look for the right pair starts from both ends.
 Time:    O(n)
-Space:    O(n)
+Space:    O(1)
 */
 std::vector<int> Solution::twoSum(std::vector<int>& nums, int target) {
-        std::vector<int> copy = nums;
         std::vector<int> answer;
         unsigned int start = 0, end = nums.size() - 1;
-        sort(nums.begin(),nums.end());
         while( start != end){
             int sum = nums[start] + nums[end];
             if(sum == target){
-                for(unsigned int i = 0; i < copy.size(); i++){
-                    if(copy[i] == nums[start] || copy[i] == nums[end]){
-                        answer.push_back(i);
-                    }
-                }
+                answer.push_back(start+1);
+                answer.push_back(end+1);
                 return answer;
             }else if(sum > target){
                 end--;
@@ -54,7 +51,7 @@ std::vector<int> Solution::twoSum(std::vector<int>& nums, int target) {
     }
 
 int main(){
-	int vec[10] = {3,4,2,5,6,9,11,23,30,32};
+	int vec[10] = {2,3,4,5,6,9,11,23,30,32};
 	int target = 10;
 	std::vector<int> nums(vec, vec+10);
 	Solution solu;
